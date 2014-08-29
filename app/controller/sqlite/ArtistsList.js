cffa13c1ca1720b304eb26ce11d9ae53e93377da
Diego García <chinopb@gmail.com>
2014-08-28 19:08:06 -0600

    Adding Chinoook sqlite database

---
+++ app/controller/sqlite/ArtistsList.js
@@ -0,0 +1,44 @@
+﻿Ext.define('PhoneGapTest.controller.sqlite.ArtistsList', {
+    extend: 'Ext.app.Controller',
+    requires: [
+        'PhoneGapTest.view.Dummy'
+    ],
+
+    config: {
+        refs: {
+            artistsList: 'artistsList'
+        },
+        control: {
+            artistsList: {
+                initialize: 'Init'
+            }
+        }
+    },
+
+    Init: function (view) {
+        if (window.sqlitePlugin) {
+            try {
+                var db = window.sqlitePlugin.openDatabase("0000000000000001.db", "1.0", "PhoneGap Demo", 200000); //{ name: "Chinook.db" }
+                db.executeSql("SELECT ArtistId, Name FROM Artist;", [], function (res) {
+                    var artistsStore = Ext.getStore('Artists');
+                    for (var index = 0; index < res.rows.length; index++) {
+                        artistsStore.add(res.rows.item(index));
+                    }
+
+                }, function (e) {
+                    //Error
+                    var nav = view.up('main');
+                    var dummy = Ext.create('dummy', { showText: JSON.stringify(e) });
+                    nav.add(dummy);
+                    view.setHtml(JSON.stringify(e));
+                });
+            }
+            catch (e) {
+                view.setHtml('Mamo');
+            }
+        }
+        else {
+            console.log('SQLite disabled');
+        }
+    }
+});
